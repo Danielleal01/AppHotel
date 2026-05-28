947fd0984a254bd270744b48d2f655f793c796b8
leald <leald@BOOK-PR38BL4AKH>
2026-05-28 19:18:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppHotel
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ AppHotel/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cause-Bold.ttf", "CauseBold");
+                    fonts.AddFont("Cause-Light.ttf", "CauseLight");
+                    fonts.AddFont("Cause-Regular.ttf", "Cause");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
