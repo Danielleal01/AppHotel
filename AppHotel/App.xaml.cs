8bb7bcd4f56b14ac1e2abe6be3feec9151fc0ded
Daniel <lealdaniel0016@gmail.com>
2026-05-28 21:04:50 -0300

    Adicão da tela Sobre

--- AppHotel/App.xaml.cs
+++ AppHotel/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.ContrataçãoHospedagem());
+            MainPage = new NavigationPage(new Views.Sobre());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
